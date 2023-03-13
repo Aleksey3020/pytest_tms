52a0a6f15b4e3600aaad5ad2314e52978a95e76a
Aleksey Adamkovich <adamkovich@yandex.ru>
2023-03-13 22:28:15 +0300

    DZ25 to check

--- selenium_dz25/auto_pract_bdd.py
+++ selenium_dz25/auto_pract_bdd.py
@@ -1,41 +1,25 @@
 from selenium_dz24.main_page import UtomationpracticeMain
 from selenium_dz24.cart_page import UtomationpracticeCart
 from selenium_dz24.login_page import UtomationpracticeLogin
-from pytest_bdd import scenarios, given, when, then, parser
+from pytest_bdd import scenarios, given, when, then
 
 scenarios('../selenium_dz25/web_shop.feature')
 
-@given('open')
+
+@given('the Automation Practice main page is displayed')
 def open_automationpractice(browser):
     main = UtomationpracticeMain(browser)
     main.open()
     main.check_for_expectation()
 
-    #main.woman_tab()
 
-    # Переход с главной строницы на страницу корзина
-
-    # main.clicking_the_shopping_cart_button()
-    # cart = UtomationpracticeCart(browser)
-    # cart.checking_cart_for_expectation()
-    # cart.cart_is_empty()
-    # cart.exit_to_main_page()
-
-    # Переход с главной строницы на страницу логина
-@when('click')
+@when('click sign in button')
 def clicking_sign_in(browser):
-    # when
     main = UtomationpracticeMain(browser)
     main.clicking_the_shopping_sign_in_button()
-    # sign_in = UtomationpracticeLogin(browser)
 
-    # sign_in.login_page_check()
-    # sign_in.visibility_already_registered()
-@then('assert')
+
+@then('error text: Invalid email address')
 def assert_text_error(browser):
-    # Then
     sign_in = UtomationpracticeLogin(browser)
     sign_in.create_an_account()
-
-    #sign_in.forgot_your_password()
-    #sign_in.exit_to_main_page()
--- selenium_dz25/web_shop.feature
+++ selenium_dz25/web_shop.feature
@@ -3,6 +3,6 @@ Feature: Create an account
   I click on the button (create an account), an error appears
 
   Scenario: Error on an empty line
-    Given open
-    When click
-    Then assert
+    Given the Automation Practice main page is displayed
+    When click sign in button
+    Then error text: Invalid email address
